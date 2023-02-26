e5a855f74856f4001661362d39d75fcfc8ab6ec5
darya.tashkina@bk.ru <darya.tashkina@bk.ru>
2023-02-26 23:30:23 +0300

    Добавьте файлы проекта.

---
+++ ProgressTable/Views/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using MonitoringProgress.Models;
+
+namespace MonitoringProgress.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
